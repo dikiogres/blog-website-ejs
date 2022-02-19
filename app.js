7022c22ae0164a7129bcc8413a74c1ecba9865e2
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-19 23:26:16 +0700

    add: post method on compose page

--- app.js
+++ app.js
@@ -19,25 +19,27 @@ app.get("/", (req, res) => {
   res.render("home", {
     startingContent: homeStartingContent
   });
-})
+});
 
 app.get("/about", (req, res) => {
   res.render("about", {
     aboutContent: aboutContent
   });
-})
+});
 
 app.get("/contact", (req, res) => {
   res.render("contact", {
     contactContent: contactContent
   });
-})
+});
 
 app.get("/compose", (req, res) => {
   res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  console.log(req.body.postTitle);
 })
-
-
 
 
 

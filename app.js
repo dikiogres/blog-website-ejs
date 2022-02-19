76c3c40a72d236026c6932d4abf0cf9ef95fed9b
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-19 23:41:11 +0700

    create: post object in compose post method

--- app.js
+++ app.js
@@ -38,8 +38,12 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  console.log(req.body.postTitle);
-})
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+});
 
 
 

d43a9ced1b911400ec1f16632216fd0fd34bb21e
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-19 23:45:56 +0700

    feat: redirect after post

--- app.js
+++ app.js
@@ -15,10 +15,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", (req, res) => {
   res.render("home", {
     startingContent: homeStartingContent
   });
+  console.log(posts);
 });
 
 app.get("/about", (req, res) => {
@@ -43,6 +46,9 @@ app.post("/compose", (req, res) => {
     content: req.body.postBody
   };
 
+  posts.push(post);
+
+  res.redirect("/");
 });
 
 
